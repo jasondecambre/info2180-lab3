353a9a283ac699ff7a8d5e3c325d8158fdfcaf32
Jason DeCambre <jasondecambre@gmail.com>
2021-10-06 11:04:58 -0500

    Completed exercise 2

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,10 +1,39 @@
 window.onload = function(){
 
+var board = document.getElementById("board"); 
+var state = []; //initializing empty array to track state
+var lastMove = ""; //tracks whose turn it is
 
-var board = document.getElementById("board");
+
 
 for(var i=0; i<9; i++){
-    board.children[i].classList.add("square")
-}
+    var sq =  board.children[i];
+    sq.classList.add("square"); //puts each of the 9 squares onto the screen
+    sq.addEventListener("click", function(){ //adding X and O
+        if(lastMove == "O" || lastMove == ""){
+            this.innerHTML = "X";
+            this.classList.add("X");
+            lastMove = "X";
+            state.push("X");
+        }
+        else{
+            this.innerHTML = "O";
+            this.classList.add("O");
+            lastMove = "O";
+            state.push("O");
+            }
+    
+    
+        }); //end event listener function
+    }; //end for loop
+
 
 }
+
+
+
+
+
+
+
+
